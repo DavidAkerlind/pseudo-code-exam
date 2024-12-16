9c1ca5d6b4bce413e3a06004613744be145d8ac4
DavidAkerlind <david.akerlind@icloud.com>
2024-12-16 15:06:27 +0100

    Started with the VG task

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# pseudo-code-exam
-Repo för paeudo kod examinerade uppgift deadline den 20/12 
+För Väl Godkänt måste du:
+
+Göra G- och VG-uppgiften Skapa ett flödesdiagram över processerna Skapa
+pseudokod som skulle fungera för båda programmen Inlämning Uppladdning av ditt
+repo på Github senast 20/12 23:59! Ta en printscreen över ditt / dina
+flödesdiagram och klistra in i readme-filen i ditt repo.
+
+![Flödeschema för "split teh nota"](image.png)
+
+[Länk till min figjam fil](https://www.figma.com/board/ujRfnkH53ehrhHVuuzfxiG/Untitled?node-id=0-1&p=f&t=UmB6glY0SzidX6w1-0)
+
+Instruktioner Pusslet går ut på att du får ett startord och ett slutord och du
+ska genom att byta ut bokstäver i ditt startord få det till ditt slutord med så
+få byten som möjligt.
+
+Regler
+
+Du får enbart byta ut en bokstav åt gången. Varje ord som bildas måste vara ett
+korrekt engelskt ord, dvs. det måste finnas i variabeln vid namn ordbok. Exempel
+
+I detta exempel så ska vi få FOUR att bli FIVE. Observera att själva spelet är
+på engelska.
+
+FOUR (startord) FOUL (Bytte ut R till L) FOOL (Bytte ut U till O) FOOT (Bytte ut
+L till T) FORT (Bytte ut O till R) FORE (Bytte ut T till E) FIRE (Bytte ut O
+till I) FIVE (Slutord)
+
+Din pseudokod måste innefatta kontroller som kontrollerar användarens input.
+Användaren får inte:
+
+Ange ett ord som inte inkluderas i varaibeln ordbok. Ändra fler eller färre
+bokstäver än 1 per runda. (Koden för detta MÅSTE abstraheras i en separat
+funktion)
---
+++ script.js
@@ -0,0 +1,57 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+INPUT variabel summa (endast siffra)
+INPUT variabel antalVänner (endast siffra, större än 0)
+INPUT variabel dricks (som ska vara ett i decimaltal)
+
+
+IF (summa > 0 eller antalVänner > 0 eller dricks > 0 eller inte är siffror THEN
+    PRINT "Alla värden måste vara siffor större än > 0"
+ENDIF
+
+SET variabel totalSumma till summa * (dricks + 1)
+
+SET variabel summaPerPerson till totalSumma / antalVänner
+
+PRINT "Varje person ska betala summaPerPerson kr"
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE] // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR"
+    SET variabel slutordOrd till "FIVE"
+    SET variabel antaFörsök till 0
+
+    WHILE gissning inte är != samma som slutOrd
+
+        Input variabel gissning till"användar input"
+
+        IF skillnaden mellan gissning och startOrd är mer änd > 1 bokstav THEN
+            print "Ogiltigt ord, du får bara ändra på en bokstav"
+        ELSE
+
+    
+            
+
+
+end function
+
+function isOneLetterApart(startOrd, guess)
+    SET variabel diffCount till 0;
+    If skullnaden mellan startOrd och gissning
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+
+
+*/
